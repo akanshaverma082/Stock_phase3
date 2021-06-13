22bc3befe427e50d8b113b1adb40e989e716577c
akanshaverma082 <akanshaverma082@gmail.com>
2021-06-13 16:52:40 +0530

    My

---
+++ src/main/java/com/telsko/demo/StockLApplication.java
@@ -0,0 +1,13 @@
+package com.telsko.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telsko/demo/StockLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telsko.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
